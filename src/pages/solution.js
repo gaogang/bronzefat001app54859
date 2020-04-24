b477439750c1583a77877db83ee84f53e1674de3
Gang Gao <ayi7456@hotmail.com>
2020-04-25 00:31:10 +0100

    Various UI updates

--- src/pages/solution.js
+++ src/pages/solution.js
@@ -1,65 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import AppBar from '@material-ui/core/AppBar';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import clsx from 'clsx';
-import Collapse from '@material-ui/core/Collapse';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PublishIcon from '@material-ui/icons/Publish';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
+import teal from '@material-ui/core/colors/teal';
 import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Switch from '@material-ui/core/Switch';
 import { makeStyles } from '@material-ui/core/styles';
-import { blue, red } from '@material-ui/core/colors';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <Typography
-        component="div"
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && <Box p={0}>{children}</Box>}
-      </Typography>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-  };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+import { Stage, Layer, Group, Rect, Text, Arrow } from 'react-konva';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -68,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
   },
 
   appBar: {
+    background: 'teal',
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
@@ -80,42 +39,20 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 5
   },
 
-  actionBar: {
-    background: 'white'
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
   },
 
-  avatar: {
-    fontSize: 12,
-    backgroundColor: red[500]
-  },
-
-  avatarDb: {
-    backgroundColor: blue[500]
-  },
-
-  buildingBlock: {
-    maxWidth: 300,
-    minWidth: 280,
-    position: 'absolute'
-  },
-
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-
-  tab: {
-      background: 'cornflowerblue'
+  info: {
+    fontFamily: 'calibri',
+    fontSize: 12
   },
 
   title: {
     flexGrow: 1,
+    fontFamily: 'Calibri'
   }
 }));
 
@@ -140,11 +77,11 @@ const addonTypes = [
 
 export default () => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-  const [expanded, setExpanded] = React.useState(false);
   const [addonDialogOpen, setAddonDialogOpen] = React.useState(false);
-  const [selectedBb, selectBb] = React.useState(null);
+  const [selectedComponent, selectComponent] = React.useState(null);
   const [selectedAddonType, selectAddonType] = React.useState('cosmosdb');
+
+  const componentWidth = 150;
 
   const mock = {
       buildingBlocks: [
@@ -154,46 +91,41 @@ export default () => {
             type: 'app',
             runtime: 'reactjs',
             status: 'pending',
-            isPrivate: true,
+            isPrivate: false,
             position: {
-                top: 250, 
+                top: 50, 
                 left: 50
             }
         },
         {
             id: 'bb 2',
-            name: 'windermeredb',
+            name: 'cosmos db',
             type: 'db',
             runtime: 'cosmosdb',
             status: 'pending',
-            isPrivate: false,
+            isPrivate: true,
             position: {
-                top: 250, 
+                top: 50, 
                 left: 550
             }
         },
+        {
+          id: 'bb 3',
+          name: 'keyvault',
+          type: 'db',
+          runtime: 'keyvault',
+          status: 'pending',
+          isPrivate: true,
+          position: {
+              top: 150, 
+              left: 550
+          }
+      },
       ]
   }
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleNewAddonClick = (bb) => {
-      if (bb.type !== 'app') {
-          return;
-      }
-
-    selectBb(bb);
-    setAddonDialogOpen(true);
-  };
-
   const handleAddonDialogClose = () => {
-        setAddonDialogOpen(false);
+    setAddonDialogOpen(false);
   }
 
   const handleNewBbClick = () => {
@@ -205,25 +137,48 @@ export default () => {
   }
 
   const handleAddonCreate = () => {
-      alert('create ' + selectedAddonType);
+    alert('create ' + selectedAddonType);
   }
 
+  const handleComponentClick = (e, component) => {
+    selectComponent(component);
+  }
 
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <AppBar position="static" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
             Solution Architecture
           </Typography>
+          <Button
+            className={classes.actionButton}
+            variant="outlined"
+            color="inherit"
+            startIcon={<AddCircleOutlineIcon />}
+            onClick={handleNewBbClick}
+          >
+            New
+          </Button>
+          <Button
+            className={classes.actionButton}
+            variant="outlined"
+            color="inherit"
+            startIcon={<ArrowForwardIosIcon />}>
+            Promote
+          </Button>
+          <Button
+            className={classes.actionButton}
+            variant="outlined"
+            color="inherit"
+            startIcon={<PublishIcon />}>
+            Publish
+          </Button>
         </Toolbar>
-        <Tabs className={classes.tab} value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Development" {...a11yProps(0)} />
-          <Tab label="Production" {...a11yProps(1)} />
-        </Tabs>
       </AppBar>
       <Dialog open={addonDialogOpen} onClose={handleAddonDialogClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">{!selectedBb ? '' : selectedBb.name}: Add-on</DialogTitle>
+        <DialogTitle id="form-dialog-title">{!selectedComponent ? '' : selectedComponent.name}: Add-on</DialogTitle>
         <DialogContent>
           <TextField
             margin="dense"
@@ -260,101 +215,56 @@ export default () => {
           </Button>
         </DialogActions>
       </Dialog>
-      <TabPanel value={value} index={0}>
-        <Toolbar className={classes.actionToolbar}>
-          <Button
-            className={classes.actionButton}
-            variant="outlined"
-            color="primary"
-            startIcon={<AddCircleOutlineIcon />}
-            onClick={handleNewBbClick}
-          >
-            New
-          </Button>
-          <Button
-            className={classes.actionButton}
-            variant="outlined"
-            color="primary"
-            startIcon={<ArrowForwardIosIcon />}>
-            Promote
-          </Button>
-          <Button
-            className={classes.actionButton}
-            variant="outlined"
-            color="primary"
-            startIcon={<PublishIcon />}>
-            Publish
-          </Button>
-        </Toolbar>
-        {
-            mock.buildingBlocks.map((bb, i) => {
+      <main className={classes.content}>
+        <div style={{width: '100%' }}>
+          <Box display='flex' flexDirection='row'>
+            <Stage width={window.innerWidth - 300 } height={window.innerHeight}>
+          <Layer>
+          {
+            mock.buildingBlocks.map((component, i) => {
                 return (
-                    <Card className={classes.buildingBlock} style={{top: bb.position.top, left: bb.position.left}}>
-                        <CardHeader
-                            avatar={
-                                <Avatar aria-label="bbtype" className={clsx(classes.avatar, {
-                                    [classes.avatarDb] : bb.type === 'db',
-                                })}>
-                                    {bb.type}
-                                </Avatar>
-                            }
-                            action={
-                                <IconButton aria-label="settings">
-                                    <MoreVertIcon />
-                                </IconButton>
-                            }
-                            title={bb.name}
-                            subheader={bb.status}
-                        />
-                        <CardActions disableSpacing>
-                            <FormControlLabel control={<Switch
-                                //checked={state.checkedA}
-                                //onChange={handleChange}
-                                name="isPrivate"
-                                inputProps={{ 'aria-label': 'Is private' }}
-                                />} 
-                                label={<Typography variant="body2" color="textSecondary">private</Typography>}
-                                labelPlacement="start"
-                            />
-                            { bb.type === 'app' &&
-                                <IconButton 
-                                    aria-label="add-ons" 
-                                    onClick={() => handleNewAddonClick(bb)} >
-                                    <AddCircleOutlineIcon />
-                                </IconButton>
-                            }
-                            <IconButton
-                                className={clsx(classes.expand, {
-                                    [classes.expandOpen]: expanded,
-                                })}
-                                onClick={handleExpandClick}
-                                aria-expanded={expanded}
-                                aria-label="show more"
-                                >
-                                <ExpandMoreIcon />
-                            </IconButton>
-                        </CardActions>
-                        <Collapse in={expanded} timeout="auto" unmountOnExit>
-                            <CardContent>
-                            <   Typography paragraph>Under construction</Typography>
-                            </CardContent>
-                        </Collapse>
-                    </Card>
-                );
+                  <Group 
+                    name={component.id}
+                    draggable={true}
+                    onClick={(e) => handleComponentClick(e, component)}>
+                    <Rect
+                      name={component.id}
+                      x={component.position.left}
+                      y={component.position.top}
+                      width={componentWidth}
+                      height={50}
+                      fill={component.isPrivate ? 'pink' : 'lightgreen'}
+                      stroke={selectedComponent && selectedComponent.id === component.id ? 'black' : 'gray'}
+                      strokeWidth={selectedComponent && selectedComponent.id === component.id ? 1.0 : 0.2}
+                    />
+                    <Text 
+                      name={component.id}
+                      text={component.name}
+                      align='center'
+                      fontSize={16}
+                      fontFamily='Calibri'
+                      x={component.position.left}
+                      y={component.position.top + 3}
+                      width={componentWidth}
+                      >
+                    </Text>
+                </Group>);
             })
         }
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-            <Toolbar className={classes.actionBar}>
-                <Button
-                    className={classes.actionButton}
-                    variant="outlined"
-                    color="primary"
-                    startIcon={<PublishIcon />}>
-                    Publish
-                </Button>
-            </Toolbar>
-        </TabPanel>
+        
+        </Layer>
+        </Stage>
+            <Box style={{background: teal[50], width: 300, padding: 10 }}>
+              <Typography className={classes.title} variant="h5" component="h2" align='center'>
+                    BronzeFat
+              </Typography>
+              <Typography className={classes.info} color="textSecondary" align='center'>
+                (App)
+              </Typography>
+            </Box>
+          </Box>
+        </div>
+      </main>
     </div>
   );
 }
