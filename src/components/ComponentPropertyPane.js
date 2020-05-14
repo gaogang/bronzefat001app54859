51f2eaca4e9f16bd93a4f3fc967335e73dfd1729
Gang Gao <gaog@Gangs-MBP.broadband>
2020-05-14 01:02:50 +0100

    Added update name dialog

--- src/components/ComponentPropertyPane.js
+++ src/components/ComponentPropertyPane.js
@@ -1,52 +1,121 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box';
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Divider from '@material-ui/core/Divider';
+import EditIcon from '@material-ui/icons/Edit';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Switch from '@material-ui/core/Switch';
+import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     info: {
         fontSize: 14
-      },
+    },
+
+    info_hl: {
+        fontSize: 14,
+        fontWeight: "bold"
+    },
     
-      title: {
+    title: {
         flexGrow: 1,
         fontSize: 16
-      }
+    }
 }));
 
 export default (props) => {
     const classes = useStyles();
     const {selectedComponent} = props;
 
+    const [open, openCloseDialog] = React.useState(false);
+
+    const handleEditName = () => {
+        openCloseDialog(true);
+    }
+
+    const handleCloseDialog = () => {
+        openCloseDialog(false);
+    }
+
+    const handleUpdateName = () => {
+        openCloseDialog(false);
+    }
+
     return (
     <div>
-        <Typography className={classes.title} align='center'>
-            {selectedComponent.name}
-        </Typography>
-        <Typography className={classes.info} color="textSecondary" align='center'>
-            ({selectedComponent.type})
-        </Typography>
-        <Divider />
+        <Dialog open={open} aria-labelledby="Name" maxWidth='md'>
+            <DialogTitle id="addon=api">Name</DialogTitle>
+            <DialogContent>
+                <TextField
+                    id="appName"
+                    label="Name"
+                    helperText="Enter the name of the selected component"
+                    variant="outlined"
+                    autoFocus
+                    />
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleCloseDialog} color="default">
+                Cancel
+            </Button>
+            <Button onClick={handleUpdateName} color="primary">
+                Update
+            </Button>
+            </DialogActions>
+        </Dialog>
         <Box style={{marginTop: 20}}>
             <Grid container spacing={1}>
-            <Grid item xs={4}>
-                <Typography className={classes.info}>
-                Status:
-                </Typography>
+                <Grid item xs={4}>
+                    <Typography className={classes.info_hl}>
+                        Name:
+                    </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography className={classes.info}>
+                        {selectedComponent.name}
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <IconButton size="small" aria-label="edit" onClick={handleEditName}>
+                        <EditIcon className={classes.info} />
+                    </IconButton>
+                </Grid>
             </Grid>
-            <Grid item xs>
-                <Typography className={classes.info}>
-                {selectedComponent.status}
-                </Typography>
+            <Grid container spacing={1}>
+                <Grid item xs={4}>
+                    <Typography className={classes.info_hl}>
+                    Type:
+                    </Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography className={classes.info}>
+                    {selectedComponent.type}
+                    </Typography>
+                </Grid>
             </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs={4}>
+                    <Typography className={classes.info_hl}>
+                    Status:
+                    </Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography className={classes.info}>
+                    {selectedComponent.status}
+                    </Typography>
+                </Grid>
             </Grid>
             <Grid container spacing={1}>
             <Grid item xs={4}>
-                <Typography className={classes.info}>
+                <Typography className={classes.info_hl}>
                 Runtime:
                 </Typography>
             </Grid>
@@ -58,7 +127,7 @@ export default (props) => {
             </Grid>
             <Grid container spacing={1}>
             <Grid item xs={4}>
-                <Typography className={classes.info}>
+                <Typography className={classes.info_hl}>
                 Resource group:
                 </Typography>
             </Grid>
@@ -70,7 +139,7 @@ export default (props) => {
             </Grid>
             <Grid container spacing={1}>
             <Grid item xs={4}>
-                <Typography className={classes.info}>
+                <Typography className={classes.info_hl}>
                 Subscription:
                 </Typography>
             </Grid>
